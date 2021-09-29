9b7bc0af8f32465f44da536643f9e65d389d2498
Matt <aboutmattlaw@gmail.com>
2021-09-29 08:17:36 -0400

    adds sort, delete, add new item all via walkthrough video

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import ListingCard from "./ListingCard";
+import NewListingForm from "./NewListingForm";
 
 
 
 
 function ListingsContainer({search}) {
 
-  const [listings, setListings ] = useState([])
-    
+  const [listings, setListings ] = useState([]);
+  const [sortBy, setSortBy] = useState("id");
+
   useEffect(() => {
     fetch('http://localhost:6001/listings')
     .then((r) => r.json())
@@ -17,6 +19,11 @@ function ListingsContainer({search}) {
   function handleDeleteListing(id){
     const updatedListingsArray = listings.filter(listing => listing.id !== id)
     setListings(updatedListingsArray) 
+  }
+
+  function handleNewListing(newListing){
+    const updatedListingsArray = [newListing, ... listings];
+    setListings(updatedListingsArray)
 
   }
 
@@ -24,6 +31,14 @@ const filteredListings = listings.filter(listing => {
   return listing.description.includes(search)
 })
 
+const sortedListings = filteredListings.sort((listingsA, listingB) => {
+  if (sortBy === "id") {
+    return listingsA.id - listingB.id
+    } else {
+      return listingsA.location.localeCompare(listingB.location)
+    }
+}
+)
 
   const listingCard = filteredListings.map((listingObj) => {
     return <ListingCard listing={listingObj} key={listingObj.id} onDeleteListing={handleDeleteListing}/>;
@@ -31,6 +46,9 @@ const filteredListings = listings.filter(listing => {
 
   return (
     <main>
+      <NewListingForm onAddListing = {handleNewListing}/>
+      <button onClick={() => setSortBy("id")}>Sort By Default</button>
+      <button onClick={() => setSortBy("location")}>Sort By Location</button>
       <ul className="cards">
         {listingCard}
       </ul>
